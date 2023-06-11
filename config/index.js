dc5ab817f919fd144354aa9042d3c4bec6e791f8
Justin <wworveaory@gapp.nthu.edu.tw>
2023-06-11 23:57:08 +0800

    123

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vuewebpack /',
+    assetsPublicPath: '/vuewebpack/',
 
     /**
      * Source Maps
--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env sh
-# 發生錯誤時執行終止指令/
+
+# 当发生错误时中止脚本
 set -e
-# 打包編譯
+
+# 构建
 npm run build
-# 移動到打包資料夾下，若你有調整的話打包後的資料夾請務必調整
+
+# cd 到构建输出的目录下
 cd dist
+
+# 部署到自定义域域名
+# echo 'www.example.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
+
+# 部署到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 部署到 https://<USERNAME>.github.io/<REPO>
 git push -f https://github.com/justin756/vuewebpack.git master:gh-pages
+
 cd -
